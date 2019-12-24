4c7596ecf155522701bfb515d5e35a05d62cb1c4
budyb <241118@student.pwr.edu.pl>
2019-12-24 16:16:50 +0100

    Add files via upload

---
+++ swiatlo.cpp
@@ -0,0 +1,67 @@
+void MyInit(void)
+	// definicja materialu
+	GLfloat mat_ambient[] = { 1.0, 1.0, 0.0, 1.0 };
+	// wspó³czynniki ka =[kar,kag,kab] dla œwiat³a otoczenia
+
+	GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	// wspó³czynniki kd =[kdr,kdg,kdb] œwiat³a rozproszonego
+
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	// wspó³czynniki ks =[ksr,ksg,ksb] dla œwiat³a odbitego               
+
+	GLfloat mat_shininess = { 20.0 };
+	// wspó³czynnik n opisuj¹cy po³ysk powierzchni
+	GLfloat light_position[] = { 0.0, 0.0, 0.0, 1.0 };
+	// po³o¿enie Ÿród³a
+	GLfloat mat_emission[] = { 1.0, 0.5, 0.5, 1.0 };
+	// emisja swiatla od materialu
+
+
+	GLfloat light_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a otoczenia
+	// Ia = [Iar,Iag,Iab]
+
+	GLfloat light_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
+	// odbicie dyfuzyjne Id = [Idr,Idg,Idb]
+
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
+	// odbicie kierunkowe Is = [Isr,Isg,Isb]
+
+	GLfloat att_constant = { 1.0 };
+	// sk³adowa sta³a ds dla modelu zmian oœwietlenia w funkcji
+	// odleg³oœci od Ÿród³a
+
+	GLfloat att_linear = { 0.05 };
+	// sk³adowa liniowa dl dla modelu zmian oœwietlenia w funkcji
+	// odleg³oœci od Ÿród³a
+
+	GLfloat att_quadratic = { 0.001 };
+	// sk³adowa kwadratowa dq dla modelu zmian oœwietlenia w funkcji
+	// odleg³oœci od Ÿród³a
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, att_constant);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, att_linear);
+	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, att_quadratic);
+	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 1.0);
+
+
+	/*************************************************************************************/
+	// Ustawienie opcji systemu oœwietlania sceny
+
+	glShadeModel(GL_SMOOTH); // w³aczenie ³agodnego cieniowania
+	glEnable(GL_LIGHTING);   // w³aczenie systemu oœwietlenia sceny
+	glEnable(GL_LIGHT0);     // w³¹czenie Ÿród³a o numerze 0
+	glEnable(GL_DEPTH_TEST); // w³¹czenie mechanizmu z-bufora
